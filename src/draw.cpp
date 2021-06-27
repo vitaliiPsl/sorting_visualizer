4dfc2f1360121d2919f84894d83e98201252ee40
vitaliiPsl <vitalii.psl.main@gmail.com>
2021-06-27 13:57:30 +0300

    draw class

---
+++ src/draw.cpp
@@ -0,0 +1,119 @@
+#include "include/draw.hpp"
+#include <iostream>
+
+Draw::Draw(sf::RenderWindow& win) : m_win(win){}
+
+std::vector<sf::RectangleShape> Draw::build_bars(std::vector<float>& data){
+    float bar_w = static_cast<float>(m_win.getSize().x / data.size());
+
+    std::vector<sf::RectangleShape> bars_vector(data.size());
+    for(int i{}; i < data.size(); i++){
+        float bar_h = data[i];
+
+        sf::RectangleShape bar(sf::Vector2f(bar_w, bar_h));
+        bar.setFillColor(sf::Color(222, 215, 215));
+        bar.setOutlineThickness(bar_w * 0.05);
+        bar.setOutlineColor(sf::Color::Black);
+        bar.setPosition(i * bar_w, m_win.getSize().y - bar_h);
+
+        bars_vector[i] = bar;
+    }
+
+    return bars_vector;
+}
+
+void Draw::draw_data(std::vector<float>& data){
+    m_win.clear(sf::Color::Black);
+    events();
+
+    auto bars = build_bars(data);
+    
+    for(auto el:bars)
+        m_win.draw(el);
+    
+    m_win.display();
+    sf::sleep(sf::milliseconds(1));    
+}
+
+void Draw::draw_sort(std::vector<float>& data, int i, int j){
+m_win.clear(sf::Color::Black);
+    events();
+
+    auto bars = build_bars(data);
+    bars[i].setFillColor(sf::Color::Red);
+    bars[j].setFillColor(sf::Color::Green);
+
+    for(auto el:bars)
+        m_win.draw(el);
+    
+    m_win.display();
+    sf::sleep(sf::milliseconds(1));
+}
+
+void Draw::draw_menu(){
+m_win.clear(sf::Color(0, 0, 0, 1));
+    events();
+
+    sf::Font f_roboto;
+    if(!f_roboto.loadFromFile("./src/font/roboto_bi.ttf")){
+        std::cout << "Fonts error. Couldn't load font" << std::endl;
+        exit(1);
+    }
+
+    sf::Text title;
+    
+    title.setString("Visualizer");
+    title.setFont(f_roboto);
+    title.setCharacterSize(50);
+    title.setFillColor(sf::Color::White);
+    title.setOrigin(
+        (title.getLocalBounds().left + title.getLocalBounds().width) / 2, 
+        (title.getLocalBounds().top - title.getLocalBounds().height) / 2
+    );
+    // title.setLetterSpacing(title.getCharacterSize() * 0.7);
+    title.setPosition(m_win.getSize().x / 2, title.getCharacterSize() + 10);
+    
+    m_win.draw(title);
+    
+    sf::Font f_roboto_r;
+    if(!f_roboto.loadFromFile("./src/font/roboto_r.ttf")){
+        std::cout << "Fonts error. Couldn't load font" << std::endl;
+        exit(1);
+    }
+
+    std::vector<sf::Text> text(6);
+    for(int i = 0; i < text.size(); i++){
+        text[i].setFont(f_roboto);
+        text[i].setCharacterSize(m_win.getSize().y / 4 / text.size());
+        text[i].setFillColor(sf::Color::White);
+       
+        text[i].setPosition(m_win.getSize().x / 2.5, m_win.getSize().y / 3 + i * text[i].getCharacterSize());
+    }
+    
+    text[0].setString("Press key:");
+    text[1].setString("Buble sort - b");
+    text[2].setString("Insertion sort - i");
+    text[3].setString("Merge sort - m");
+    text[4].setString("Shuffle - s");
+    text[5].setString("Exit - esc");
+    
+    
+    for(auto el:text)
+        m_win.draw(el);
+            
+    m_win.display();
+    std::cout << "Displayed" << std::endl;
+}
+
+void Draw::events(){
+    sf::Event event;
+    while(m_win.pollEvent(event)){
+        switch(event.type){
+        case sf::Event::Closed:
+            m_win.close();
+            exit(1);
+        default:
+            break;
+        }
+    }
+}
---
+++ src/include/draw.hpp
@@ -0,0 +1,23 @@
+#if !defined(Draw_H)
+#define Draw_H
+
+#include "../SFML/Graphics.hpp"
+
+class Draw{
+    sf::RenderWindow& m_win;
+
+public:
+    Draw(sf::RenderWindow& win);
+
+    std::vector<sf::RectangleShape> build_bars(std::vector<float>& data);
+
+    void draw_data(std::vector<float>& data);
+
+    void draw_sort(std::vector<float>& data, int i, int j);
+
+    void draw_menu();
+
+    void events();
+};
+
+#endif // Draw_H
